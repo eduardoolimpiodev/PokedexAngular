94f8e378f80a2a40a5165e857879489195b18b2b
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-03-29 20:08:42 -0300

    add: routing modules

--- pokedex/src/app/pages/routing.module.ts
+++ pokedex/src/app/pages/routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
+//Componentes
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./pages/pages.module').then(p => p.PagesModule)
+    component: HomeComponent
   }
 ];
 
