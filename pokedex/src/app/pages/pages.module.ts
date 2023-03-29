685bb34281a28d5bf26fe12a0f5151456c6e0a6e
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-03-29 20:17:14 -0300

    Add: Component details

--- pokedex/src/app/pages/pages.module.ts
+++ pokedex/src/app/pages/pages.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 
+//Module Routing
+import { RoutingModule } from './routing.module';
+import { DetailsComponent } from './details/details.component';
+
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    DetailsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RoutingModule
   ]
 })
 export class PagesModule { }
--- pokedex/src/app/pages/routing.module.ts
+++ pokedex/src/app/pages/routing.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 
 //Componentes
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
+
+
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'details',
+    component: DetailsComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class RoutingModule { }
