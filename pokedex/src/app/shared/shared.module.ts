7fa21367d41e09f0aaa9306cf452aababbdeeb18
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-03-29 20:42:30 -0300

    add: logo

--- pokedex/src/app/shared/shared.module.ts
+++ pokedex/src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 //Componente
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
+import { PokeSearchComponent } from './poke-search/poke-search.component';
 
 
 
 @NgModule({
   declarations: [
-    PokeHeaderComponent
+    PokeHeaderComponent,
+    PokeSearchComponent
   ],
   exports: [
     PokeHeaderComponent
