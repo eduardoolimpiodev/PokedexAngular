5424bcb8513a5aeef5fe21da7b57f8f3498cbc3c
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-03-30 17:57:11 -0300

    add: get method

--- pokedex/src/app/service/poke-api.service.ts
+++ pokedex/src/app/service/poke-api.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeApiService {
 
-  constructor() { }
+  private url: string = 'https://pokeapi.co/api/v2/ability/?offset=0&limit=100';
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  get apiListAllPokemons():Observable<any>{
+    return this.http.get<any>(this.url).pipe(
+      
+    )
+  }
+
 }
