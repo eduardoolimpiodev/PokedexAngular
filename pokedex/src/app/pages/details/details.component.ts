c6acff2f1ee47594cfd46f6cba382fd3491e1ed8
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-03-31 22:19:30 -0300

    add details service

--- pokedex/src/app/pages/details/details.component.ts
+++ pokedex/src/app/pages/details/details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
 import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
@@ -12,22 +13,29 @@ export class DetailsComponent implements OnInit {
   private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
   private urlName : string = 'https://pokeapi.co/api/v2/pokemon-species';
 
+  public pokemon: any;
+
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private pokeApiService: PokeApiService
   ) { }
 
   ngOnInit(): void {
-    this.pokemon;
+    this.getPokemon;
   }
 
-  get pokemon(){
+  get getPokemon(){
     
     const id = this.activatedRoute.snapshot.params['id'];
-    const pokemon = this.pokeApiService.apiGetPokemon(`$this.urlPokemon/${id}`);
-    const name = this.pokeApiService.apiGetPokemon(`$this.urlName/${id}`);
+    const pokemon = this.pokeApiService.apiGetPokemon(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemon(`${this.urlName}/${id}`);
 
-    return console.log(id);
+    return forkJoin([pokemon, name]).subscribe(
+      res => {
+        this.pokemon = res;
+      }
+    );
   }
 
 }
