9e29d138ced459f4611105ae19d4bd2c4e49be31
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-03-31 22:09:46 -0300

    add: service url Polemon

--- pokedex/src/app/pages/details/details.component.ts
+++ pokedex/src/app/pages/details/details.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon'
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) { }
+
+  ngOnInit(): void {
+    this.pokemon;
+  }
+
+  get pokemon(){
+    
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemon(`$this.urlPokemon/${id}`);
+    
+
+    return console.log(id);
+  }
 
 }
